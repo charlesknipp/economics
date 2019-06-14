826a51ea92a8bd7bfaa4bc4770a7aad5ab01c011
classicharlie <cknipp@asu.edu>
2019-06-14 14:38:36 -0700

    6/14 update

--- the regression script/regression.py
+++ the regression script/regression.py
@@ -12,28 +12,61 @@ register_matplotlib_converters()
 
 
 # this is a very rudimentary login system so we can each use our own api keys
-user = str(input('new script, who dis? '))
+name = str(input('\nnew script, who dis?'))
+answer = str(input('%s, u have a FRED api key?' % name))
 
-if user == 'charlie':
-    fred = Fred(api_key='ecae2fc8d6c684847525a828ae7a3ab8')
-    x = 1
+yay = ['yes', 'yeah', 'yup', 'y', 'yuh', 'of course', 'duh', 'yay']
+nay = ['no', 'nah', 'nope', 'n', 'naw', 'negative', 'kinda', 'well...']
 
-elif user == 'noah':
-    fred = Fred(api_key='9c9998bd36a2bdbcf4e13c63b0a5edaf')
-    x = 2
+for response in range(0, len(yay)):
+    if answer.lower() == yay[response]:
+        key = str(input('enter ur FRED api key: '))
+        fred = Fred(api_key=key)
+        x = 1
+        break
 
-elif user == 'anon':
-    fred = Fred(api_key='')
-    x = 3
+    elif answer.lower() == nay[response]:
+        print('u should get one using the link in the instructions')
+        x = 0
+        break
 
-else:
-    print('fuq u')
-    user = input('u bitch, tell me one more goddamn time. ')
-    x = 0
+    else:
+        x = 0
 
 
 # this conditional houses our work
-if x != 1:
+if x == 1:
+    # I eventually want to iterate this process so you can manually input any
+    # amount of datasets as long as they exist in FRED
+
+    '''
+    txt = str(input('which FRED datasets u want? '))
+    txt_nospaces = txt.replace(' ', '')
+    datasets = txt_nospaces.split(',')
+    '''
+
+    # now this is where I think I fucked up but I don't know for sure
+    # I think I can use a .append to create a list of series, but with the
+    # way the api calls each series I have to be careful about iterating
+
+    '''
+    ser = fred.get_series(datasets[0]).to_frame()
+    df_init = ser.to_frame(name=datasets[0])
+
+    for s in datasets:
+        if datasets.index(s) == 0:
+            continue
+
+        ser_loop = fred.get_series(datasets[s])
+        df_loop = ser_loop.to_frame(name=datasets[s])
+
+        if datasets.index(s) == 1:
+            df1 = df_init.join(df_loop)
+
+        elif datasets.index(s) > 1:
+            df = df1.join(df_loop)
+    '''
+
     # take the series from FRED and convert it to a ts data frame
     ser1 = fred.get_series('GDP')
     df1 = ser1.to_frame(name='GDP')
@@ -66,13 +99,19 @@ if x != 1:
     print('\nthese are the coefficients:\n', coefficients)
 
     i = 1    # i := independent vairables
-    while i < 2:
+
+    '''
+    i = len(datasets)
+    ''' # this bit is for when I complete the user input functionality
+
+    if i < 2:
         if lmadj.pvalues[i] < .05:
-            print('\nreject the null')
+            print('reject the null')
 
         else:
-            print('\nlmao nice try fa66ot')
+            print('lmao u thought')
 
 
-else:
-    print('\nu hav a lil pp')
+elif x == 0:
+    print('that ain\'t it cheif\nu prolly derive smallest')
+    x = 0
